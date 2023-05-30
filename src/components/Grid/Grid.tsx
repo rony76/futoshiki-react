671b5c694f2955b7b58ca9b7f5556c4cc51762f2
Rony Cesana <9611986+rony76@users.noreply.github.com>
2023-05-30 10:14:24 +0200

    Clean up prepopulated content and add dummy Grid component

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Grid.css';
+
+interface GridProps {}
+
+const Grid: FC<GridProps> = () => (
+  <div className="Grid">
+    Grid Component here!
+  </div>
+);
+
+export default Grid;
