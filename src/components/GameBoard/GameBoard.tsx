c94286e3f8253054c4129c30659b889ce3b066f9
Rony Cesana <87707125+ronytw@users.noreply.github.com>
2023-05-31 20:39:38 +0200

    Support moving selected cell with keyboard

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -3,17 +3,46 @@ import './GameBoard.css';
 import ValueRow from "../ValueRow/ValueRow";
 import GapRow from "../GapRow/GapRow";
 import {Game} from "../../model/Game";
-import {Coordinates} from "../../model/Coordinates";
+import {Coordinates, Direction} from "../../model/Coordinates";
 import {ActiveCellContext} from '../../ActiveCellContext';
 
 interface GameBoardProps {
     game: Game
 }
 
+function getArrowKeyDirection(key: string): Direction | null {
+    switch (key) {
+        case 'ArrowLeft':
+            return 'left';
+        case 'ArrowRight':
+            return 'right';
+        case 'ArrowUp':
+            return 'up';
+        case 'ArrowDown':
+            return 'down';
+        default:
+            return null;
+    }
+}
+
 const GameBoard: FC<GameBoardProps> = ({game}: GameBoardProps) => {
     const [activeCell, setActiveCell] = useState<Coordinates | null>(null);
 
     let cells = Array<JSX.Element>();
+
+    const captureKeys = (e: React.KeyboardEvent) => {
+        if (!activeCell) return;
+
+        const dir: Direction | null = getArrowKeyDirection(e.key);
+
+        if (dir !== null) {
+            if (activeCell.canMove(dir, game.size)) {
+                setActiveCell(activeCell.getNextGoing(dir, game.size))
+            }
+        } else if (e.key === 'Escape') {
+            setActiveCell(null);
+        }
+    }
 
     for (let row = 1; row <= game.size; row++) {
         cells = cells.concat(<ValueRow game={game} row={row} key={"val" + row}/>)
@@ -23,7 +52,7 @@ const GameBoard: FC<GameBoardProps> = ({game}: GameBoardProps) => {
     }
 
     return (
-        <table className="GameBoard">
+        <table className="GameBoard" tabIndex={0} onKeyDown={captureKeys}>
             <tbody>
             <ActiveCellContext.Provider value={[activeCell, setActiveCell]}>
                 {cells}
